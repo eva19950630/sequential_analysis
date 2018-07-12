52f0d78038716054e98f26543993414a37bb711d
Ray <xic7682100@gmail.com>
2018-07-12 17:50:03 +0800

    PigSavior part: Add UI and test js call python.

--- js/sequentialAnalysis.js
+++ js/sequentialAnalysis.js
@@ -81,8 +81,54 @@ var dataObj = [];
 function load_Webdisk() {
     $.getJSON("webdisk_inputdata.json", function(data) {
         dataObj = data;
-        for (var i in dataObj) {
-            console.log(dataObj[i].account);
-        }
+        // for (var i in dataObj) {
+            // console.log(dataObj[i].account);
+        // }
     });
 }
+
+function load_pigSaviorData(){
+    $.getJSON("pigSavior_inputdata.json", function(data){
+        dataObj = data;
+        var users = [], scenes = [];
+        for (var i in dataObj){
+            if(!users.includes(dataObj[i].user_id)){
+                users.push(dataObj[i].user_id);
+            }
+            if(!scenes.includes(dataObj[i].game_scene)){
+                scenes.push(dataObj[i].game_scene);
+            }
+        }
+        users.sort((a, b) => a - b);
+        scenes.sort((a, b) => a.slice(5)-b.slice(5));
+
+        userlist = "", scenelist = "";
+        for (var i in users){
+            // console.log(users[i]);
+            userlist += "<option value = "+users[i]+">"+users[i]+"</option>";
+        }
+        document.getElementById('userList').innerHTML = userlist;
+        gamestage = "";
+        for (var i in scenes){
+            scenelist += "<option value = "+scenes[i]+">"+scenes[i]+"</option>";
+        }
+        document.getElementById('sceneList').innerHTML = scenelist;
+    });
+}
+
+function selectDatas(){
+    var userSelect = document.getElementById("userList");
+    var user = userSelect.options[userSelect.selectedIndex].value;
+    var sceneSelect = document.getElementById("sceneList");
+    var scene = sceneSelect.options[sceneSelect.selectedIndex].value;
+
+    $.ajax({
+        type: "POST",
+        url: "./python/selectData.py",
+        data: { param: "123"}
+    }).done(function(output){
+        console.log(output);
+    });
+}
+
+
---
+++ python/selectData.py
@@ -0,0 +1,15 @@
+import sys, getopt
+import json
+
+# print sys.argv
+
+# json_data = sys.argv[1]
+# with open(json_data, 'r') as file:
+#     jsonData = json.loads(file.readline())
+# file.close()
+
+# person = sys.argv[2]
+# scene = sys.argv[3]
+
+# return person + '\n' + scene
+return sys.argv
